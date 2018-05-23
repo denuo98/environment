1def22e07ebc6c638c4ed60694bb4ecb0f858755
ginnerpeace <ginnerpeace@live.com>
2018-05-23 19:54:00 +0800

    呵呵

---
+++ bash/redis-rdm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# run redis desktop manager crossover version
+
+exec /home/ginnerpeace/.cxoffice/RedisDesktopManager/desktopdata/cxmenu/Desktop.C^5E3A_users_crossover_Desktop/RedisDesktopManager.lnk >/tmp/redis-desktop-manager.output 2>&1 &
